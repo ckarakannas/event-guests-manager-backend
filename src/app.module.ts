4a5e3d572622efc0b0d178d54dd0bafbab13646f
Chris Karakannas <christodoulos.karakannas@gmail.com>
2022-03-27 14:32:51 +0100

    Added throttling module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { GuestsModule } from './guests/guests.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 import ormConfig from './config/orm.config';
 import ormConfigProd from './config/orm.config.prod';
 import jwtConfig from './config/jwt.config';
@@ -22,6 +23,14 @@ import jwtConfig from './config/jwt.config';
     TypeOrmModule.forRootAsync({
       useFactory:
         process.env.NODE_ENV === 'development' ? ormConfig : ormConfigProd,
+    }),
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        ttl: config.get('THROTTLE_TTL'),
+        limit: config.get('THROTTLE_LIMIT'),
+      }),
     }),
     EventsModule,
     AuthModule,
